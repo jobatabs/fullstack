2ff5a2680ea6b7eeb759f27056a073ac162a3bf6
jobatabs <2370315+jobatabs@users.noreply.github.com>
2024-10-29 21:03:38 +0200

    d1.14

--- part1/anekdootit/src/App.jsx
+++ part1/anekdootit/src/App.jsx
@@ -11,13 +11,28 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  const [highest, setHighest] = useState(0)
+
+  const handleVotes = () => {
+    const newPoints = [...points]
+    newPoints[selected] += 1
+    setHighest(points.indexOf(Math.max(...points)))
+    setPoints(newPoints)
+  }
 
   return (
-    <div>
-      {anecdotes[selected]}
-    </div>
+    <>
+      <h1>anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button onClick={() => handleVotes()}>vote</button>
+      <button onClick={() => { setSelected(Math.floor(Math.random() * 8)) }}>next anecdote</button>
+      <h1>anecdote with the most votes</h1>
+      <p>{anecdotes[highest]}</p>
+    </>
   )
 }
 
