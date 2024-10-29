0072b5acc0c649ef6d8ff0725752c2da67a6524e
jobatabs <2370315+jobatabs@users.noreply.github.com>
2024-10-29 19:58:57 +0200

    1c

--- part1/tutorial/src/App.jsx
+++ part1/tutorial/src/App.jsx
@@ -1,24 +1,40 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
+      <Display counter={counter}/>
+
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 
 export default App
